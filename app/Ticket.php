25d90abe5d8b39624a6e618e8e140c64f297abd6
tortas2719 <uzziel_56@hotmail.com>
2018-11-01 23:56:17 -0600

    Relations on models :)

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+  public function device()
+    {
+        return $this->hasOne('App\Device', 'id');
+    }
+
+  public function service()
+    {
+        return $this->hasOne('App\Service', 'id');
+    }
 }
