8000c87582f8b31e2f2105d40b39b77317c80595
ald06 <ald_rz_06@hotmail.com>
2018-11-02 01:23:18 -0600

    Update seeders [catalogos terminados]

--- database/seeds/LocationSeeder.php
+++ database/seeds/LocationSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Location;
 class LocationSeeder extends Seeder
 {
     /**
@@ -11,6 +11,349 @@ class LocationSeeder extends Seeder
      */
     public function run()
     {
-        //
+      //Campus I Edificio A Planta baja
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Secretaría Técnica';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Planta baja
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Secretaría Técnica';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Planta baja
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Secretaría Técnica';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Planta baja
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Red de Biología y Conservacíon de Vertebrados';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Planta baja
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Red de Biología y Conservacíon de Vertebrados';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Laboratorio';
+      $location->save();
+      //Campus I Edificio A Planta baja
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Red de Ecología Funcional';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Planta baja
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Secretaría Técnica';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Segundo piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Red de Biodiversidad Y Sistemática';
+      $location->nivel = 'Segundo piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Segundo piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Red de Ambiente y Sustentabilidad';
+      $location->nivel = 'Segundo piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Segundo piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Red de Biodiversidad y Sistemática';
+      $location->nivel = 'Segundo piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Segundo piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Red de Biología y Conservación de Vertebrados';
+      $location->nivel = 'Segundo piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Segundo piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Red de Ecoetología';
+      $location->nivel = 'Segundo piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Segundo piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Red de Ecología Funcional';
+      $location->nivel = 'Segundo piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio A Segundo piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'A';
+      $location->departamento = 'Coordinación de TIC';
+      $location->nivel = 'Segundo piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio B Primer piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'B';
+      $location->departamento = 'Órgano Interno de Control';
+      $location->nivel = 'Primer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio B Primer piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'B';
+      $location->departamento = 'Red de Ecoetología';
+      $location->nivel = 'Primer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio B Primer piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'B';
+      $location->departamento = 'Oficina de Enlace con la Sociedad';
+      $location->nivel = 'Primer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio B Segundo piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'B';
+      $location->nivel = 'Segundo piso';
+      $location->departamento = 'Red de Ambiente y Sustentabilidad';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio B Tercer piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'B';
+      $location->departamento = 'Red de Ambiente y Sustentabilidad';
+      $location->nivel = 'Tercer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio B Tercer piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'B';
+      $location->departamento = 'Secretaría Técnica';
+      $location->nivel = 'Tercer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio C Planta Baja
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'C';
+      $location->departamento = 'Red de Biología Evolutiva';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio C Primer piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'C';
+      $location->departamento = 'Red de Ecoetología';
+      $location->nivel = 'Primer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio C Segundo piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'C';
+      $location->departamento = 'Red de Biología Evolutiva';
+      $location->nivel = 'Segundo piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio D Planta baja
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'D';
+      $location->departamento = 'Red de Manejo Biotecnológico de Recursos';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus I Edificio E Primer piso
+      $location= new Location;
+      $location->campus = 'I';
+      $location->edificio = 'E';
+      $location->departamento = 'Secretaría Posgrados';
+      $location->nivel = 'Primer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus II Edificio F Planta baja
+      $location= new Location;
+      $location->campus = 'II';
+      $location->edificio = 'F';
+      $location->departamento = 'Secretaría Técnica';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus II Edificio F Planta baja
+      $location= new Location;
+      $location->campus = 'II';
+      $location->edificio = 'F';
+      $location->departamento = 'Secretaría Técnica';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Laboratorio';
+      $location->save();
+      //Campus II Edificio F Planta baja
+      $location= new Location;
+      $location->campus = 'II';
+      $location->edificio = 'F';
+      $location->departamento = 'Red de Ecología Funcional';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus II Edificio F Primer piso
+      $location= new Location;
+      $location->campus = 'II';
+      $location->edificio = 'F';
+      $location->departamento = 'Red de Ecología Funcional';
+      $location->nivel = 'Primer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus II Edificio F Primer piso
+      $location= new Location;
+      $location->campus = 'II';
+      $location->edificio = 'F';
+      $location->departamento = 'Red de Interacciones Multitróficas';
+      $location->nivel = 'Primer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus II Edificio F Segundo piso
+      $location= new Location;
+      $location->campus = 'II';
+      $location->edificio = 'F';
+      $location->departamento = 'Red de Ambiente y Sustentabilidad';
+      $location->nivel = 'Segundo piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus II Edificio F segundo piso
+      $location= new Location;
+      $location->campus = 'II';
+      $location->edificio = 'F';
+      $location->departamento = 'Red de Ecología Funcional';
+      $location->nivel = 'segundo piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus II Edificio G Primer piso
+      $location= new Location;
+      $location->campus = 'II';
+      $location->edificio = 'G';
+      $location->departamento = 'Red de Ambiente y Sustentabilidad';
+      $location->nivel = 'Primer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus II Edificio G Primer piso
+      $location= new Location;
+      $location->campus = 'II';
+      $location->edificio = 'G';
+      $location->departamento = 'Red de Ecología Funcional';
+      $location->nivel = 'Primer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus III Edificio A Planta baja
+      $location= new Location;
+      $location->campus = 'III';
+      $location->edificio = 'A';
+      $location->departamento = 'Dirección General';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus III Edificio A Planta baja
+      $location= new Location;
+      $location->campus = 'III';
+      $location->edificio = 'A';
+      $location->departamento = 'Laboratorio de Catación de Café';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus III Edificio A Primer piso
+      $location= new Location;
+      $location->campus = 'III';
+      $location->edificio = 'A';
+      $location->departamento = 'Dirección General';
+      $location->nivel = 'Primer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus III Edificio B Planta baja
+      $location= new Location;
+      $location->campus = 'III';
+      $location->edificio = 'B';
+      $location->departamento = 'Red de Estudios Moleculares Avanzados';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus III Edificio B Planta baja
+      $location= new Location;
+      $location->campus = 'III';
+      $location->edificio = 'B';
+      $location->departamento = 'Red de Estudios Moleculares Avanzados';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Laboratorio';
+      $location->save();
+      //Campus III Edificio B Primer piso
+      $location= new Location;
+      $location->campus = 'III';
+      $location->edificio = 'B';
+      $location->departamento = 'Secretaría Técnica';
+      $location->nivel = 'Primer piso';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus III Edificio C Planta baja
+      $location= new Location;
+      $location->campus = 'III';
+      $location->edificio = 'C';
+      $location->departamento = 'Red de Manejo Biorracional de Plagas y Vectores';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus III Edificio C Planta baja
+      $location= new Location;
+      $location->campus = 'III';
+      $location->edificio = 'C';
+      $location->departamento = 'Red de Manejo Biotecnológico de Recursos';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
+      //Campus III Edificio D Planta baja
+      $location= new Location;
+      $location->campus = 'III';
+      $location->edificio = 'D';
+      $location->departamento = 'Red de Manejo Biorracional de Plagas y Vectores';
+      $location->nivel = 'Planta baja';
+      $location->areaTrabajo = 'Cubículo';
+      $location->save();
     }
 }
--- database/seeds/TypeSeeder.php
+++ database/seeds/TypeSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Type;
 
 class TypeSeeder extends Seeder
 {
@@ -11,6 +12,49 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+      //Desktop
+      $type = new Type;
+      $type->nombre = 'Desktop';
+      $type->save();
+      //Laptop
+      $type = new Type;
+      $type->nombre = 'Laptop';
+      $type->save();
+      //Smartphone
+      $type = new Type;
+      $type->nombre = 'Smartphone';
+      $type->save();
+      //Telefono IP
+      $type = new Type;
+      $type->nombre = 'Telefono IP';
+      $type->save();
+      //Tablet
+      $type = new Type;
+      $type->nombre = 'Tablet';
+      $type->save();
+      //Impresora
+      $type = new Type;
+      $type->nombre = 'Impresora';
+      $type->save();
+      //Router
+      $type = new Type;
+      $type->nombre = 'Router';
+      $type->save();
+      //Switch
+      $type = new Type;
+      $type->nombre = 'Switch';
+      $type->save();
+      //Access point
+      $type = new Type;
+      $type->nombre = 'Access point';
+      $type->save();
+      //Secuenciador
+      $type = new Type;
+      $type->nombre = 'Secuenciador';
+      $type->save();
+      //Servidor
+      $type = new Type;
+      $type->nombre = 'Servidor';
+      $type->save();
     }
 }
